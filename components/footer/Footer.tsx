fabaa2e48d4150cf55693da640fa0741fe059ccb
AfifSatrio <afifsatria2108@gmail.com>
2026-03-05 19:30:41 +0700

    add tiktok

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -12,16 +12,28 @@ const Footer = ({ settings }: { settings: Settings }) => {
         <div className="grid md:grid-cols-[1fr_2fr_1fr] grid-cols-1 gap-10 mx-15">
             <div className="flex flex-col gap-4 items-center md:items-start">
                 <Image src={"/logo/footer-logo.svg"} alt={"footer logo"} width={150} height={100} className="scale-90 lg:scale-100 justify-end"/>
-                <a
-                    href="https://www.instagram.com/alira.interior?igsh=MXJudW90bDJkcDkzaw=="
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="group flex items-center gap-2.5 w-fit"
-                    >
-                    <span className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center group-hover:border-alira group-hover:bg-alira/10 transition-all duration-300">
-                        <i className="ri-instagram-line text-sm text-gray-400 group-hover:text-alira transition-colors duration-300" />
-                    </span>
-                </a>
+                <div className="flex flex-row gap-2">
+                    <a
+                        href="https://www.instagram.com/alira.interior?igsh=MXJudW90bDJkcDkzaw=="
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="group flex items-center gap-2.5 w-fit"
+                        >
+                        <span className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center group-hover:border-alira group-hover:bg-alira/10 transition-all duration-300">
+                            <i className="ri-instagram-line text-sm text-gray-400 group-hover:text-alira transition-colors duration-300" />
+                        </span>
+                    </a>
+                    <a
+                        href="https://www.tiktok.com/@alirainterior_07?_r=1&_t=ZS-94R2dTWY4rS"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="group flex items-center gap-2.5 w-fit"
+                        >
+                        <span className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center group-hover:border-alira group-hover:bg-alira/10 transition-all duration-300">
+                            <i className="ri-tiktok-fill text-sm text-gray-400 group-hover:text-alira transition-colors duration-300" />
+                        </span>
+                    </a>
+                </div>
             </div>
             <div className="flex flex-col font-poppins gap-2">
                 <h1 className="font-bold text-lg">Punya Pertanyaan?</h1>
